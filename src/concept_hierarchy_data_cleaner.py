3d4e9434c02da77d14f0b04bd516e18ff1efa9de
1995YogeshSharma <yogesh.sharma@research.iiit.ac.in>
2019-07-16 20:48:44 +0530

    file added for python parse and some data modified in dataset concept hierarchy.

--- src/concept_hierarchy_data_cleaner.py
+++ src/concept_hierarchy_data_cleaner.py
@@ -13,9 +13,44 @@ Output format -
 [item-name]
 [item-path-separated-by-spaces]
 """
+import sys
 
 def main():
-    pass
+	file_name = sys.argv[1]
+	out_file_name = sys.argv[2]
+
+	try:
+		fin = open(file_name, 'r')
+		fout = open(out_file_name, 'w')
+
+	except:
+		print "ERROR: Enter correct file name"
+		sys.exit()
+
+
+	is_item_line = 1
+	line = ''
+
+	for line in fin.readlines():
+		if is_item_line:
+			# modify and write item line
+			it_name = line.split('.')[1].strip()
+			fout.write(it_name.replace(' ', '_') + '\n')
+			is_item_line = 0
+		else:
+			# modify and write path line
+			full_path = line.split('=')[1]
+			path_array = full_path.split('/')
+
+			for category in path_array:
+				fout.write(category.strip().replace(' ', '_') + ' ')
+			fout.write('\n\n')
+
+			is_item_line = 1
+
+	fin.close()
+	fout.close()
+
 
 if __name__ == '__main__':
     main()
